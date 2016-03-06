f2204330ff9900e701c58b817824f1cf9ddd8668
notyetawizard <notyetawizard@gmail.com>
2016-03-06 17:29:17 -0600

    stuff

--- main.lua
+++ main.lua
@@ -10,14 +10,6 @@ local math = require "math"
     -Correct randomness; it's not true right now
 --]]
 
---Temp crap here!
-str = 6
-dex = 15
-con = 12
-int = 16
-wis = 14
-cha = 13
---
 
 --Get the modifier value from a base stat
 function statMod(stat)
@@ -28,8 +20,42 @@ function roll(die)
     return math.random(1,die)
 end
 
-function skillCheck(stat,bonus)
-    x = roll(20) + statMod(stat)
+function skillCheck(who,skill,bonus)
+    x = roll(20) + statMod(who.stats[who.skills[skill][1]])
+    if who.skills[skill][2] == true then x = x + who.stats.prof end
+    --just incase of other bonuses!
     if bonuses ~= nil then x = x + bonus end
     return x
 end
+
+function initSkill(who,name,stat)
+    if who.skills == nil then who.skills = {} end
+    who.skills[name] = {stat, false, 0}
+end
+    
+--Temp crap here!
+me = {}
+--Stats
+me.stats = {
+    str = 6,
+    dex = 15,
+    con = 12,
+    int = 16,
+    wis = 14,
+    cha = 13,
+    prof = 2
+}
+--Skills (and saves)
+me.skills = {
+    strength = {"str", false, 0},
+    dexterity = {"dex", true, 0},
+    --
+    acrobatics = {"dex", true, 0},
+    animal_handling = {"wis", false, 0},
+    arcana = {"int", false, 0},
+    athletics = {"str", false, 0},
+    --
+    rapier = {"dex", true, 0}
+}
+
+initSkill(me, "deception","cha")
